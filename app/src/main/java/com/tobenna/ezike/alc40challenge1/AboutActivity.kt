10ab85d1a4bd566c2a96b9df002c37575db05171
Tobenna Ezike <eziketobenna@Ezikes-MacBook-Pro.local>
2019-07-14 09:41:33 +0100

    Initial commit

---
+++ app/src/main/java/com/tobenna/ezike/alc40challenge1/AboutActivity.kt
@@ -0,0 +1,61 @@
+package com.tobenna.ezike.alc40challenge1
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_about.*
+
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        web_view.apply {
+            settings.javaScriptEnabled = true
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    view?.loadUrl(getString(R.string.about_url))
+                    progress_bar.visibility = View.VISIBLE
+                    return true
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    progress_bar.visibility = View.GONE
+                }
+            }
+
+        }
+
+        if (isConnected) {
+            web_view.loadUrl(getString(R.string.about_url))
+        } else {
+            Snackbar.make(
+                root, "Please check your internet connection",
+                Snackbar.LENGTH_LONG
+            )
+        }
+    }
+
+}
+
+val Context.isConnected: Boolean
+    get() {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return connectivityManager
+            .activeNetworkInfo?.isConnected == true && connectivityManager.activeNetworkInfo != null
+    }
---
+++ app/src/main/java/com/tobenna/ezike/alc40challenge1/ProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.tobenna.ezike.alc40challenge1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
